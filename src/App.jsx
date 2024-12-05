9b0fbaff424416f074467929f149b2d2b4021752
test <test@UGSERVER.com>
2024-12-05 15:56:05 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import './index.css'
+function App() {
+  return (
+   <div>
+    <h1 className='text-4xl text-blue-500'>sudha</h1>
+   </div>
+  )
+}
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",  // Ensures Tailwind scans all your React files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
